cd568c6aa4d7b091777cd460850e1f32b98b4980
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2021-02-28 06:59:17 +0800

    Initial Commit

---
+++ MyFueliOS/MyFueliOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFueliOSApp.swift
+//  MyFueliOS
+//
+//  Created by Thinh Tran on 28/2/21.
+//
+
+import SwiftUI
+
+@main
+struct MyFueliOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
