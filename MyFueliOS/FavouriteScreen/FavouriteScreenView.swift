e68caf1ef3aedb1efb084afd4efd241e8596767b
Hung914 <51743068+Hung914@users.noreply.github.com>
2021-03-13 22:18:12 +0800

    add favourites screen

--- MyFueliOS/FavouriteScreen/FavouriteScreenView.swift
+++ MyFueliOS/FavouriteScreen/FavouriteScreenView.swift
@@ -1,26 +1,34 @@
 //
-//  FavouriteScreenView.swift
+//  FavouriteScreen.swift
 //  MyFueliOS
 //
-//  Created by Hung Tran on 12/3/21.
+//  Created by Hung Tran on 13/3/21.
 //
 
 import SwiftUI
 
 struct FavouriteScreenView: View {
+    @State private var currentTag = "Price"
     var body: some View {
         NavigationView {
                 List {
                     VStack {
-                        Text("Favourite here")
+                              Picker(selection: $currentTag, label: Text("Sort by price or by distance")) {
+                                  Text("Price").tag("Price")
+                                  Text("Distance").tag("Distance")
+                              }
+                              .pickerStyle(SegmentedPickerStyle())
+
+                        
                           }
+
 
             }.navigationTitle("Favourites")
         }
     }
 }
 
-struct FavouriteScreenView_Previews: PreviewProvider {
+struct FavouriteScreen_Previews: PreviewProvider {
     static var previews: some View {
         FavouriteScreenView()
     }
