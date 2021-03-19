8414fbcf108a324cfe9589ab901e24361a8b52d7
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2021-03-19 21:47:16 +0800

    Add custom fonts

---
+++ MyFueliOS/Resources/Fonts/Font.swift
@@ -0,0 +1,23 @@
+//
+//  Font.swift
+//  MyFueliOS
+//
+//  Created by Thinh Tran on 19/3/21.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    public static func FHACondFrenchNC(size: CGFloat) -> Font {
+        return Font.custom("FHA Condensed French NC", size: size)
+    }
+    
+    public static func AmericanCaptain(size: CGFloat) -> Font {
+        return Font.custom("American Captain", size: size)
+    }
+    
+    public static func FjallaOne(size: CGFloat) -> Font {
+        return Font.custom("FjallaOne-Regular", size: size)
+    }
+}
