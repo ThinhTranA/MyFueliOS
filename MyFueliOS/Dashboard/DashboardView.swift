b8580fcd7caed3e040a4eeda8b5cc72f65f63ade
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2021-03-21 07:14:43 +0800

    tidy up DashboardView

--- MyFueliOS/Dashboard/DashboardView.swift
+++ MyFueliOS/Dashboard/DashboardView.swift
@@ -19,49 +19,77 @@ struct DashboardView: View {
         ZStack {
             Color.gray.opacity(0.1)
             VStack{
-                Group{
-                    Text("ABCDEFGHIJKLM123456").font(.AmericanCaptain(size: 20))
-                    Text("ABCDEFGHIJKLM123456").font(.FHACondFrenchNC(size: 20))
-                    Text("ABCDEFGHIJKLM123456").font(.FjallaOne(size: 20))
-                    Text("ABCDEFGHIJKLM123456").font(.system(size: 20))
-                }
-           
-                HStack{
-                    Text("PERTH PRICE RANGE")
-                    Spacer()
-                }.padding(EdgeInsets(top: 4, leading: 16, bottom: 4, trailing: 0))
-         
-                PriceRangeView(lowest: viewModel.lowestPrice, highest: viewModel.highestPrice, range: viewModel.priceRange)
-                
+                priceRangeView
                 Spacer()
-                AveragePriceView(averagePrice: viewModel.averagePrice, description: viewModel.product.description)
-               
+                averagePriceView
                 Spacer()
-           
-                Group{
-                    Text("Top 3 cheapest stations")
-                    VStack {
-                        ForEach(viewModel.cheapest3Stations) { station in
-                            CheapestStationRowView(station: station)
-                                .padding(EdgeInsets(top: 4, leading: 8, bottom: 16, trailing: 8))
-                        }
-                    }
-                    
-                    Spacer()
-                }
-                          
-                SelectFuelTypeView(fuelType: viewModel.product.description)
+                cheapestStationsView
+                selectFuelTypeView
                     .padding(EdgeInsets(top: 4, leading: 16, bottom: 16, trailing: 16))
             }
            
         }
         
     }
-}
-
-struct CheapestStationRowView: View {
-    var station : PetrolStation
-    var body: some View {
+    
+    
+    private var priceRangeView: some View {
+        Group{
+            HStack{
+                Text("PERTH PRICE RANGE")
+                Spacer()
+            }.padding(EdgeInsets(top: 4, leading: 16, bottom: 4, trailing: 0))
+            
+            HStack{
+                VStack {
+                    Text("Lowest")
+                    Text(viewModel.lowestPrice)
+                }.padding(EdgeInsets(top: 4, leading: 16, bottom: 4, trailing: 0))
+                Spacer()
+                Image(systemName: "arrow.left")  .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 60, height: 30)
+                Spacer()
+                VStack {
+                    Text("Range")
+                    Text(viewModel.priceRange)
+                }
+                Spacer()
+                Image(systemName: "arrow.right")  .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 60, height: 30)
+                Spacer()
+                VStack {
+                    Text("Highest")
+                    Text(viewModel.highestPrice)
+                }.padding(EdgeInsets(top: 4, leading: 0, bottom: 4, trailing: 16))
+            }
+        }
+    }
+    
+    private var averagePriceView: some View {
+        Group{
+            Text("TODAY'S AVERAGE")
+            Text(viewModel.averagePrice)
+            Text(viewModel.product.description)
+        }
+    }
+    
+    private var cheapestStationsView: some View {
+        Group{
+            Text("Top 3 cheapest stations")
+            VStack {
+                ForEach(viewModel.cheapest3Stations) { station in
+                    cheapestStationRowView(station: station)
+                        .padding(EdgeInsets(top: 4, leading: 8, bottom: 16, trailing: 8))
+                }
+            }
+            
+            Spacer()
+        }
+    }
+    
+    private func cheapestStationRowView(station: PetrolStation) -> some View {
         HStack{
             Text(station.price)
             Spacer()
@@ -77,69 +105,26 @@ struct CheapestStationRowView: View {
             }
         }
     }
-}
-
-struct AveragePriceView: View {
-    var averagePrice: String
-    var description: String
-    var body: some View {
-        Text("TODAY'S AVERAGE")
-        Text(averagePrice)
-        Text(description)
-    }
-}
-
-struct PriceRangeView: View {
-    var lowest: String
-    var highest: String
-    var range: String
-    var body: some View {
-        HStack{
-            VStack {
-                Text("Lowest")
-                Text(lowest)
-            }.padding(EdgeInsets(top: 4, leading: 16, bottom: 4, trailing: 0))
-            Spacer()
-            Image(systemName: "arrow.left")  .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 60, height: 30)
-            Spacer()
-            VStack {
-                Text("Range")
-                Text(range)
-            }
-            Spacer()
-            Image(systemName: "arrow.right")  .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 60, height: 30)
-            Spacer()
-            VStack {
-                Text("Highest")
-                Text(highest)
-            }.padding(EdgeInsets(top: 4, leading: 0, bottom: 4, trailing: 16))
+    
+    private var selectFuelTypeView: some View {
+            ZStack{
+                Color.white
+                HStack{
+                    Text("Fuel Type").font(.title3).opacity(0.6)
+                    Spacer()
+                    Text(viewModel.product.description).font(.title3)
+                    Image(systemName: "chevron.right")
+                }.padding(EdgeInsets(top: 4, leading: 16, bottom: 4, trailing: 16))
+            }.frame( height: 60, alignment: .center)
+            .cornerRadius(20)
+       
+        .onTapGesture {
+            print("change fuel type tapped")
         }
     }
 }
 
-struct SelectFuelTypeView: View {
-    var fuelType: String
-    var body: some View{
-        ZStack{
-            Color.white
-            HStack{
-                Text("Fuel Type").font(.title3).opacity(0.6)
-                Spacer()
-                Text(fuelType).font(.title3)
-                Image(systemName: "chevron.right")
-            }.padding(EdgeInsets(top: 4, leading: 16, bottom: 4, trailing: 16))
-        }.frame( height: 60, alignment: .center)
-        .cornerRadius(20)
-   
-    .onTapGesture {
-        print("change fuel type tapped")
-    }
-    }
-}
+
 
 struct DashboardView_Previews: PreviewProvider {
     static var previews: some View {
