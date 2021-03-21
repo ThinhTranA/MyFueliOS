601fb785b150d57b29673609ee099cfd1f82ca06
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2021-03-21 23:44:39 +0800

    Add Remove item from favorite List.

--- MyFueliOS/Favourite/FavouriteView.swift
+++ MyFueliOS/Favourite/FavouriteView.swift
@@ -9,18 +9,38 @@ import SwiftUI
 
 struct FavouriteView: View {
     @ObservedObject var viewModel = FavouriteViewModel()
+    @State var isEditing = false
     @State private var currentTag = "Price"
+    private let favService = CachedService.shared
+  
     var body: some View {
+     
         NavigationView {
                 List {
                     ForEach(viewModel.favStations){ station in
                         NavigationLink (destination: StationDetailView(station: station)) {
                             StationRowView(petrolStation: station)
-                    }
-            }   
+                        }
+                    }.onDelete(perform: { indexSet in
+                        let stationToDelete = indexSet.map { self.viewModel.favStations[$0] }[0]
+                        favService.RemoveFromFavourites(station: stationToDelete)
+                    })
+                    
         }.onAppear{
             viewModel.fetchFavouriteStations()
-        }.navigationTitle("Favourites")
+        }
+        .listStyle(PlainListStyle())
+        .navigationTitle("Favourites")
+                
+        //Binding List edit mode to isEditing var instead of using EditButton()
+        .environment(\.editMode, .constant(self.isEditing ? EditMode.active : EditMode.inactive)).animation(Animation.spring())
+        .navigationBarItems(trailing: Button(action: {
+            isEditing.toggle()
+        }, label: {
+            Image(systemName: "wrench")
+                    .resizable()
+                    .frame(width: 28, height: 28)
+        }))
     }
 }
 }
