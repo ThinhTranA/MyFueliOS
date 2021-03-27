c76975b5643552339e077fda1997a89e3f07f0e4
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2021-03-27 22:33:55 +0800

    Make StationsMapView layout more readable.

--- MyFueliOS/Map/SelectedStationDetailView.swift
+++ MyFueliOS/Map/SelectedStationDetailView.swift
@@ -12,59 +12,62 @@ struct SelectedStationDetailView: View {
     @State private var stationDetailIsShowing = false
     var body: some View {
       
-        VStack{
-            RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(Color.gray.opacity(0.5))
-                          .frame(width: 48, height: 6)
-            if let station = station {
-                StationRowView(petrolStation: station)
-            }
-            Button(action: {
-                //Open apple map
-                let url = URL(string: "http://maps.apple.com/?address=\(station!.address.replacingOccurrences(of: " ", with: "+"))")
-                if let url = url {
-                    UIApplication.shared.open(url)
-                }
-            }) {
-              
-                HStack(spacing: 10) {
-                    Spacer()
-                    Text("Navigate").font(.FHACondFrenchNC(size: 18))
-                    Image(systemName: "arrow.triangle.turn.up.right.circle")
-                        .resizable()
-                        .frame(width: 28, height: 28, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    Spacer()
-                  }
-               
-                .frame( height: 42, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .cornerRadius(20)
-            }.padding(EdgeInsets(top: -4, leading: 32, bottom: 8, trailing: 32))
-                
-        
-        }
-        .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
-        .background(Color.white)
-        .cornerRadius(32, corners: [.topLeft, .topRight])
-        .shadow(color: Color.black.opacity(0.3),
-            radius: 5, x: 3,y: -3)
-        .gesture(DragGesture().onEnded{ value in
-            if(value.translation.height < 0) { //up
-                stationDetailIsShowing = true
-            }
-            else if (value.translation.height > 0 ){
-                station = nil
-            }
-           
-        }).sheet(isPresented: $stationDetailIsShowing, onDismiss: {}, content: {
-            NavigationView {
+        VStack {
+            Spacer()
+            VStack{
+                RoundedRectangle(cornerRadius: 25, style: .continuous)
+                    .fill(Color.gray.opacity(0.5))
+                              .frame(width: 48, height: 6)
                 if let station = station {
-                    StationDetailView(station: station)
+                    StationRowView(petrolStation: station)
                 }
-            }.navigationViewStyle(StackNavigationViewStyle())
+                Button(action: {
+                    //Open apple map
+                    let url = URL(string: "http://maps.apple.com/?address=\(station!.address.replacingOccurrences(of: " ", with: "+"))")
+                    if let url = url {
+                        UIApplication.shared.open(url)
+                    }
+                }) {
+                  
+                    HStack(spacing: 10) {
+                        Spacer()
+                        Text("Navigate").font(.FHACondFrenchNC(size: 18))
+                        Image(systemName: "arrow.triangle.turn.up.right.circle")
+                            .resizable()
+                            .frame(width: 28, height: 28, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        Spacer()
+                      }
+                   
+                    .frame( height: 42, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(20)
+                }.padding(EdgeInsets(top: -4, leading: 32, bottom: 8, trailing: 32))
+                    
+            
+            }
+            .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
+            .background(Color.white)
+            .cornerRadius(32, corners: [.topLeft, .topRight])
+            .shadow(color: Color.black.opacity(0.3),
+                radius: 5, x: 3,y: -3)
+            .gesture(DragGesture().onEnded{ value in
+                if(value.translation.height < 0) { //up
+                    stationDetailIsShowing = true
+                }
+                else if (value.translation.height > 0 ){
+                    station = nil
+                }
+               
+            }).sheet(isPresented: $stationDetailIsShowing, onDismiss: {}, content: {
+                NavigationView {
+                    if let station = station {
+                        StationDetailView(station: station)
+                    }
+                }.navigationViewStyle(StackNavigationViewStyle())
         })
+        }
     }
     
     private func getHeight() -> CGFloat {
--- MyFueliOS/Map/StationsMapView.swift
+++ MyFueliOS/Map/StationsMapView.swift
@@ -12,84 +12,86 @@ struct StationsMapView: View {
     @State var selectedPetrolStation: PetrolStation?
     @State var mapFocusToUserLocation = true
     @State var selectedProduct = Product.UnleadedPetrol
-    
+
     var body: some View {
-        
+
         ZStack {
             MapView(stations: $viewModel.perthStations, selectedStation: $selectedPetrolStation, shouldFocusUserLocation: $mapFocusToUserLocation)
-            
+
             if selectedPetrolStation != nil {
-                VStack{
-                    Spacer()
-                    SelectedStationDetailView(station: $selectedPetrolStation)
-                }
+                SelectedStationDetailView(station: $selectedPetrolStation)
             }
 
-            HStack {
-                VStack(spacing: 24){
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack(spacing: 8) {
-                            ForEach(Product.allCases, id: \.self) { p in
-                                Button(action: {
-                                    viewModel.fetchPetrolStations(by: p)
-                                    //TODO only update this if success
-                                    selectedProduct = p
-                                }){
-                                    Text(p.description)
-                                        .fontWeight(.bold)
-                                        .font(.FjallaOne(size: 14))
-                                        .padding(8)
-                                        .background(
-                                            p == selectedProduct ? Color.SteamGold : Color.white)
-                                        .cornerRadius(40)
-                                        .foregroundColor(.black)
-                                        .padding(4)
-                                }
-
-                            }
-                        }
-                    }
-                    
-                    HStack {
-                        Spacer()
-                        Button(action: {
-                            mapFocusToUserLocation = true
-                        }, label: {
-                            
-                            //TODO: this is not working, fix this bug.
-                            if( mapFocusToUserLocation){
-                                Image(systemName: "location.fill")
-                                    .font(.system(size: 24))
-                                    .frame(width: 42, height: 42)
-                                    .foregroundColor(Color.blue)
-                            } else {
-                                Image(systemName: "location")
-                                    .font(.system(size: 24))
-                                    .frame(width: 42, height: 42)
-                                    .foregroundColor(Color.blue)
-                                    
-                            }
-                                
-                        })
-                        .background(Color.white)
-                        .cornerRadius(20)
-                        .shadow(color: Color.black.opacity(0.3),
-                            radius: 3, x: 3,y: 3)
-                    }.padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 16))
-                    Spacer()
-                }
+            VStack(spacing: 24) {
+                fuelTypeSelectionView
+                focusToUserLocationButtonView
+                Spacer()
             }.padding(EdgeInsets(top: 12, leading: 2, bottom: 0, trailing: 2))
-          
-            if(viewModel.isLoading){
+
+            if (viewModel.isLoading) {
                 LoadingView()
             }
         }
+    }
+
+    private var fuelTypeSelectionView: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 8) {
+                ForEach(Product.allCases, id: \.self) { p in
+                    Button(action: {
+                        viewModel.fetchPetrolStations(by: p)
+                        //TODO only update this if success
+                        selectedProduct = p
+                    }) {
+                        Text(p.description)
+                                .fontWeight(.bold)
+                                .font(.FjallaOne(size: 14))
+                                .padding(8)
+                                .background(
+                                        p == selectedProduct ? Color.SteamGold : Color.white)
+                                .cornerRadius(40)
+                                .foregroundColor(.black)
+                                .padding(4)
+                    }.shadow(color: Color.black.opacity(0.3),
+                            radius: 3, x: 3, y: 3)
+                }
+            }
+        }
+    }
+
+    private var focusToUserLocationButtonView: some View {
+        HStack {
+            Spacer()
+            Button(action: {
+                mapFocusToUserLocation = true
+            }, label: {
+
+                //TODO: this is not working, fix this bug.
+                if (mapFocusToUserLocation) {
+                    Image(systemName: "location.fill")
+                            .font(.system(size: 24))
+                            .frame(width: 42, height: 42)
+                            .foregroundColor(Color.blue)
+                } else {
+                    Image(systemName: "location")
+                            .font(.system(size: 24))
+                            .frame(width: 42, height: 42)
+                            .foregroundColor(Color.blue)
+
+                }
+
+            })
+                    .background(Color.white)
+                    .cornerRadius(20)
+                    .shadow(color: Color.black.opacity(0.3),
+                            radius: 3, x: 3, y: 3)
+        }.padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 16))
     }
 }
 
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        StationsMapView( selectedPetrolStation: PetrolStation.mockPetrolStations[0])
+        StationsMapView(selectedPetrolStation: PetrolStation.mockPetrolStations[0])
     }
 }
