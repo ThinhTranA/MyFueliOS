86638c87cd428a414a71cdb9854c80d7feabd89e
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2021-03-27 23:10:55 +0800

    Fix crash of map on load.

--- MyFueliOS/UIKitViews/MapView.swift
+++ MyFueliOS/UIKitViews/MapView.swift
@@ -63,6 +63,9 @@ struct MapView: UIViewRepresentable {
         }
 
         //To avoid updating annotations of the same data on the map
+        if mapView.annotations.count < 1 {
+            return
+        }
         if let currentFirstStationAnnotation = mapView.annotations[0] as? StationAnnotation {
             //TODO: cannot find self in scope crash trait here
             if(currentFirstStationAnnotation.fuelType == stations[0].fuelType) {
