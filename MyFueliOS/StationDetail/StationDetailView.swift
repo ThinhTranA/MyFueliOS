19dd5596ecacd139f088de83e9b0407a2958ace0
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2021-03-28 21:53:19 +0800

    Refactor buttons view on station detail screen.

--- MyFueliOS/StationDetail/StationDetailView.swift
+++ MyFueliOS/StationDetail/StationDetailView.swift
@@ -10,19 +10,107 @@ import MapKit
 
 struct StationDetailView: View {
     @State var station: PetrolStation
+    @State var isInFav: Bool = false
+    @State var isAddrCopied: Bool = false
+    private let favService = CachedService.shared
 
     var body: some View {
         GeometryReader { geometry in
-            VStack{
-                StationDetailMap(station: station).frame(maxHeight: geometry.size.height * 0.5)
-              
-                StationDetailPrice(station: station)
-                StationDetailOptions(station: station)
-                Button("Copy Address"){
-                    UIPasteboard.general.string = station.address
-                }
+            VStack(spacing: 0) {
+                StationDetailMap(station: station).frame(maxHeight: geometry.size.height * 0.4)
+                VStack {
+                    buttonsList
+                    Divider()
+
+                    StationDetailPrice(station: station)
+
+                }.padding()
                 Spacer()
             }.navigationTitle(station.tradingName)
+        }
+    }
+
+    private var buttonsList: some View {
+        let btnMaxHeight = CGFloat(40)
+        let btnMaxWidth = CGFloat(120)
+        let icSize = CGFloat(18)
+
+        return HStack(spacing: 12) {
+            Button(action: {
+                if (isInFav) {
+                    favService.RemoveFromFavourites(station: station)
+                } else {
+                    favService.AddToFavourites(station: station)
+                }
+                isInFav.toggle()
+            }) {
+
+                HStack(spacing: 4) {
+                    Image(systemName: "star")
+                            .resizable()
+                            .frame(width: icSize, height: icSize, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(isInFav ? .white : .red)
+                    Text(isInFav ? "In Favourite" : "Favourite")
+                            .multilineTextAlignment(.center)
+                            .fixedSize(horizontal: false, vertical: true)
+                            .foregroundColor(isInFav ? .white : .red)
+                }
+
+                        .frame(minWidth: 0, maxWidth: btnMaxWidth, minHeight: 28, maxHeight: btnMaxHeight)
+                        .overlay(
+                                RoundedRectangle(cornerRadius: 12)
+                                        .stroke(Color.red, lineWidth: 1.6)
+                        )
+                        .background(isInFav ? Color.red : Color.white)
+                        .cornerRadius(12)
+            }.onAppear {
+                isInFav = favService.GetFavourites().contains(station.address)
+            }
+
+            Button(action: {
+                UIPasteboard.general.string = station.address
+                isAddrCopied = true
+            }) {
+
+                HStack(spacing: 4) {
+
+                    Image(systemName: "doc.on.doc")
+                            .resizable()
+                            .frame(width: icSize, height: icSize, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(isAddrCopied ? .white : .green)
+
+                    Text(isAddrCopied ? "Address Copied" : "Address").foregroundColor(isAddrCopied ? .white : .green)
+                            .fixedSize(horizontal: false, vertical: true)
+                }
+
+                        .frame(minWidth: 0, maxWidth: btnMaxWidth, minHeight: 28, maxHeight: btnMaxHeight)
+                        .overlay(
+                                RoundedRectangle(cornerRadius: 12)
+                                        .stroke(Color.green, lineWidth: 1.6)
+                        )
+            }.background(isAddrCopied ? Color.green : Color.white)
+                    .cornerRadius(12)
+
+            Button(action: {
+                let url = URL(string: "http://maps.apple.com/?address=\(station.address.replacingOccurrences(of: " ", with: "+"))")
+                if let url = url {
+                    UIApplication.shared.open(url)
+                }
+            }) {
+
+                HStack(spacing: 4) {
+
+                    Image(systemName: "car")
+                            .resizable()
+                            .frame(width: icSize, height: icSize, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.SteamGold)
+                    Text("Direction").foregroundColor(.SteamGold)
+                }.frame(minWidth: 0, maxWidth: btnMaxWidth, minHeight: 28, maxHeight: btnMaxHeight)
+                        .overlay(
+                                RoundedRectangle(cornerRadius: 12)
+                                        .stroke(Color.SteamGold, lineWidth: 1.6)
+                        )
+            }
         }
     }
 }
