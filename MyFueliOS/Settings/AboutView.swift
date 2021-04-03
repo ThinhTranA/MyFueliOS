6afbb884733c9d6044f92f184f281faef295a445
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2021-04-03 13:32:03 +0800

    Fix AboutView text cut off on iphone 8 & 7

--- MyFueliOS/Settings/AboutView.swift
+++ MyFueliOS/Settings/AboutView.swift
@@ -13,56 +13,58 @@ struct AboutView: View {
     @State var isShowingMailDisableAlert = false
 
     var body: some View {
-        VStack (alignment: .leading, spacing: 32){
-            VStack (alignment: .leading, spacing: 16){
-                Text("Thanks for using Fuel Lens").font(.title)
-                Text("Currently Fuel Lens is only showing fuel prices in Perth and areas in Western Australia.").font(.title3)
-                Text("If you run into any issues or have a suggestion, please send me an email at myfuellens@gmail.com by using the email support button below.").font(.title3)
-            }
-     
-            Button(action: {
-                if MFMailComposeViewController.canSendMail(){
-                    self.isShowingMailView.toggle()
-                } else {
-                    self.isShowingMailDisableAlert.toggle()
+        ScrollView {
+            VStack (alignment: .leading, spacing: 32){
+                VStack (alignment: .leading, spacing: 12){
+                    Text("Thanks for using Fuel Lens").font(.title)
+                    Text("Currently Fuel Lens is only showing fuel prices in Perth and areas in Western Australia.").font(.title3)
+                    Text("If you run into any issues or have a suggestion, please send me an email at myfuellens@gmail.com by using the email support button below.").font(.title3)
                 }
-             
-            }, label: {
-            HStack(spacing: 10) {
-                  Spacer()
-                  Text("Email support")
-                  Image(systemName: "envelope")
-                      .resizable()
-                      .frame(width: 28, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                  Spacer()
-                }
-              .frame( height: 42, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-              .background(Color.blue)
-              .foregroundColor(.white)
-              .cornerRadius(22)
-        })  .disabled(!MFMailComposeViewController.canSendMail())
-            .alert(isPresented: $isShowingMailDisableAlert) {
-                       Alert(title: Text("Fuel Lens"), message: Text("Unable to send email from this device"), dismissButton: .default(Text("Ok")))
-                   }
-            .sheet(isPresented: $isShowingMailView) {
-                      MailView(result: $result) { composer in
-                          composer.setSubject("Fuel Lens enquiry")
-                          composer.setToRecipients(["myfuellens@gmail.com"])
+         
+                Button(action: {
+                    if MFMailComposeViewController.canSendMail(){
+                        self.isShowingMailView.toggle()
+                    } else {
+                        self.isShowingMailDisableAlert.toggle()
+                    }
+                 
+                }, label: {
+                HStack(spacing: 10) {
+                      Spacer()
+                      Text("Email support")
+                      Image(systemName: "envelope")
+                          .resizable()
+                          .frame(width: 28, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                      Spacer()
+                    }
+                  .frame( height: 42, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                  .background(Color.blue)
+                  .foregroundColor(.white)
+                  .cornerRadius(22)
+            })  .disabled(!MFMailComposeViewController.canSendMail())
+                .alert(isPresented: $isShowingMailDisableAlert) {
+                           Alert(title: Text("Fuel Lens"), message: Text("Unable to send email from this device"), dismissButton: .default(Text("Ok")))
+                       }
+                .sheet(isPresented: $isShowingMailView) {
+                          MailView(result: $result) { composer in
+                              composer.setSubject("Fuel Lens enquiry")
+                              composer.setToRecipients(["myfuellens@gmail.com"])
+                          }
                       }
-                  }
 
-            VStack (alignment: .leading, spacing: 16) {
-                Text("Made possible by").font(.title)
-                Text("Fuel Watch RSS").font(.title3)
-                Text("SwiftUI").font(.title3)
-                Text("SwiftXMLParser").font(.title3)
-            }
+                VStack (alignment: .leading, spacing: 16) {
+                    Text("Made possible by").font(.title)
+                    Text("Fuel Watch RSS").font(.title3)
+                    Text("SwiftUI").font(.title3)
+                    Text("SwiftXMLParser").font(.title3)
+                }
 
-        Spacer()
-       }
-    
-       .padding()
-       .navigationTitle("About")
+            Spacer()
+           }
+        
+           .padding()
+            .navigationTitle("About")
+        }
 
     }
 
